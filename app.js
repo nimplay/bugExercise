65ae48d39daf050c6a035ab3810d5e04f1a9bdff
nimplay <nimrod7day@gmail.com>
2022-06-17 11:36:51 -0400

    Exercise in pair

---
+++ app.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form');
+const name = document.getElementById('f-name');
+const email = document.getElementById('f-mail');
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+  formControl.className = 'form-name error';
+}
+
+function setSuccessFor(input) {
+  const formControl = inpot.parentElement;
+  formControl.className = 'form-name success';
+}
+
+function isEmail(email) {
+  return /^[a-z0-9.!#$%&'*+=?^_`{|}~-]+@[a-z0-9](?:[a-zA-Z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/.test(email);
+}
+
+function checkImputs() {
+  const usernameValue = name.value.trim();
+  const usermailValue = email.value.trim();
+
+  if (usernameValue === '') {
+    setErrorFor(name, 'Full name cannot be in blank');
+  } else {
+    setSuccessFor(email);
+  }
+  if (usermailValue === '') {
+    setErrorFor(email, 'Email address cannot be in blank');
+  } else if (!isEmail(usermailValue)) {
+    setErrorFor(mail, 'Email address is not valid');
+  } else {    
+    setSuccessFor(name);
+  }
+}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkImputs();
+});
